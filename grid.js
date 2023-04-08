61e8c897fd3c45b46e9d96df00dbd1c277d43887
DjoleXPet <djolepetrovic610@gmail.com>
2023-04-08 22:13:14 +0200

    Added basic functionality, only one matrix

---
+++ grid.js
@@ -0,0 +1,142 @@
+window.addEventListener('load', addGrid);
+
+function addGrid() {
+    var grid_size = 15;
+// var x_axis_distance_grid_lines = 5;
+// var y_axis_distance_grid_lines = 5;
+var x_axis_starting_point = { number: 1, suffix: '' };
+var y_axis_starting_point = { number: 1, suffix: '' };
+
+var canvas = document.getElementById("mainCanvas");
+var ctx = canvas.getContext("2d");
+
+var canvas_width = canvas.width;
+var canvas_height = canvas.height;
+
+var num_lines_x = Math.floor(canvas_height/grid_size);
+var num_lines_y = Math.floor(canvas_width/grid_size);
+
+var x_axis_distance_grid_lines = Math.floor(num_lines_x / 2);
+var y_axis_distance_grid_lines = Math.floor(num_lines_y / 2);
+
+
+// Draw grid lines along X-axis
+for(var i=0; i<=num_lines_x; i++) {
+    ctx.beginPath();
+    ctx.lineWidth = 1;
+    
+    // If line represents X-axis draw in different color
+    if(i == x_axis_distance_grid_lines) 
+        ctx.strokeStyle = "#000000";
+    else
+        ctx.strokeStyle = "#e9e9e9";
+    
+    if(i == num_lines_x) {
+        ctx.moveTo(0, grid_size*i);
+        ctx.lineTo(canvas_width, grid_size*i);
+    }
+    else {
+        ctx.moveTo(0, grid_size*i+0.5);
+        ctx.lineTo(canvas_width, grid_size*i+0.5);
+    }
+    ctx.stroke();
+}
+
+// Draw grid lines along Y-axis
+for(i=0; i<=num_lines_y; i++) {
+    ctx.beginPath();
+    ctx.lineWidth = 1;
+    
+    // If line represents X-axis draw in different color
+    if(i == y_axis_distance_grid_lines) 
+        ctx.strokeStyle = "#000000";
+    else
+        ctx.strokeStyle = "#e9e9e9";
+    
+    if(i == num_lines_y) {
+        ctx.moveTo(grid_size*i, 0);
+        ctx.lineTo(grid_size*i, canvas_height);
+    }
+    else {
+        ctx.moveTo(grid_size*i+0.5, 0);
+        ctx.lineTo(grid_size*i+0.5, canvas_height);
+    }
+    ctx.stroke();
+}
+
+// Translate to the new origin. Now Y-axis of the canvas is opposite to the Y-axis of the graph. So the y-coordinate of each element will be negative of the actual
+ctx.translate(y_axis_distance_grid_lines*grid_size, x_axis_distance_grid_lines*grid_size);
+//ctx.translate(canvas_width/2, canvas_height/2);
+
+// ctx.translate(400, 300);
+
+// Ticks marks along the positive X-axis
+for(i=1; i<(num_lines_y - y_axis_distance_grid_lines); i++) {
+    ctx.beginPath();
+    ctx.lineWidth = 1;
+    ctx.strokeStyle = "#000000";
+
+    // Draw a tick mark 6px long (-3 to 3)
+    ctx.moveTo(grid_size*i+0.5, -3);
+    ctx.lineTo(grid_size*i+0.5, 3);
+    ctx.stroke();
+
+    // Text value at that point
+    ctx.font = '9px Arial';
+    ctx.textAlign = 'start';
+    ctx.fillText(x_axis_starting_point.number*i + x_axis_starting_point.suffix, grid_size*i-2, 15);
+}
+
+// Ticks marks along the negative X-axis
+for(i=1; i<y_axis_distance_grid_lines; i++) {
+    ctx.beginPath();
+    ctx.lineWidth = 1;
+    ctx.strokeStyle = "#000000";
+
+    // Draw a tick mark 6px long (-3 to 3)
+    ctx.moveTo(-grid_size*i+0.5, -3);
+    ctx.lineTo(-grid_size*i+0.5, 3);
+    ctx.stroke();
+
+    // Text value at that point
+    ctx.font = '9px Arial';
+    ctx.textAlign = 'end';
+    ctx.fillText(-x_axis_starting_point.number*i + x_axis_starting_point.suffix, -grid_size*i+3, 15);
+}
+
+// Ticks marks along the positive Y-axis
+// Positive Y-axis of graph is negative Y-axis of the canvas
+for(i=1; i<(num_lines_x - x_axis_distance_grid_lines); i++) {
+    ctx.beginPath();
+    ctx.lineWidth = 1;
+    ctx.strokeStyle = "#000000";
+
+    // Draw a tick mark 6px long (-3 to 3)
+    ctx.moveTo(-3, grid_size*i+0.5);
+    ctx.lineTo(3, grid_size*i+0.5);
+    ctx.stroke();
+
+    // Text value at that point
+    ctx.font = '9px Arial';
+    ctx.textAlign = 'start';
+    ctx.fillText(-y_axis_starting_point.number*i + y_axis_starting_point.suffix, 8, grid_size*i+3);
+}
+
+// Ticks marks along the negative Y-axis
+// Negative Y-axis of graph is positive Y-axis of the canvas
+for(i=1; i<x_axis_distance_grid_lines; i++) {
+    ctx.beginPath();
+    ctx.lineWidth = 1;
+    ctx.strokeStyle = "#000000";
+
+    // Draw a tick mark 6px long (-3 to 3)
+    ctx.moveTo(-3, -grid_size*i+0.5);
+    ctx.lineTo(3, -grid_size*i+0.5);
+    ctx.stroke();
+
+    // Text value at that point
+    ctx.font = '9px Arial';
+    ctx.textAlign = 'start';
+    ctx.fillText(y_axis_starting_point.number*i + y_axis_starting_point.suffix, 8, -grid_size*i+3);
+}
+}
---
+++ main.js
@@ -0,0 +1,119 @@
+window.addEventListener('load', mainFunc);
+
+
+document.querySelectorAll('table input').forEach(item => {
+  item.addEventListener('change', event => {
+    mainFunc();
+  })
+})
+
+function mainFunc() {
+    /*const poly = document.getElementById('mainPoly');
+    points = [200, 10, 250, 190, 360, 210];
+    poly.setAttribute('points', `${points[0]},${points[1]} ${points[2]},${points[3]} ${points[4]},${points[5]}`);*/
+
+    
+
+    const grid_size = 15
+
+    // const ogCoords = [[0 * grid_size,0 * grid_size],[5 * grid_size,0 * grid_size],
+    //                  [5 * grid_size ,-5 * grid_size],[0 * grid_size,-5 * grid_size]]
+
+    const ogCoords = [[0 * grid_size,0 * grid_size, 1],[5 * grid_size,0 * grid_size, 1],
+                      [5 * grid_size ,-5 * grid_size, 1],[0 * grid_size,-5 * grid_size, 1]]
+
+    var c = document.getElementById("mainCanvas");
+    var ctx = c.getContext('2d');
+    ctx.clearRect(-c.width, -c.height, 2*c.width, 2*c.height);
+    ctx.reset()
+   // ctx.setTransform(1, 0, 0, 1, 0, 0);
+
+    addGrid();
+    
+    ctx.fillStyle = '#f00';
+    ctx.beginPath();
+    // ctx.moveTo(0, 0);
+    // ctx.lineTo(-50,0);
+    // ctx.lineTo(-50, 50);
+    // ctx.lineTo(0, 50);
+    ctx.moveTo(ogCoords[0][0], ogCoords[0][1]);
+    ctx.lineTo(ogCoords[1][0], ogCoords[1][1]);
+    ctx.lineTo(ogCoords[2][0], ogCoords[2][1]);
+    ctx.lineTo(ogCoords[3][0], ogCoords[3][1]);
+    
+    ctx.closePath();
+    ctx.fill();
+
+    // za transliranje treba da se pomnozi sa grid_size
+    // matrix = [[1,0,grid_size*5],
+    //           [0,1,-grid_size*8]]
+    // matrix = [[Math.cos(Math.PI/4) ,-Math.sin(Math.PI/4) ,grid_size*5],
+    //           [Math.sin(Math.PI/4) , Math.cos(Math.PI/4) ,-grid_size*8],
+    //           [                 0,                     0 ,           1 ]]
+    const newCoords = new Array(4)
+
+    let matrix = loadMatrix();
+    matrix[0][2] = matrix[0][2]*grid_size
+    matrix[1][2] = -matrix[1][2]*grid_size
+    
+ //   console.log(matrix)
+    for (let i = 0; i < newCoords.length; i++) {
+        newCoords[i] = new Array(3)
+        newCoords[i] = calculateTransformation(matrix, ogCoords[i])  
+    }
+
+    ctx.fillStyle = '#0f0';
+    ctx.beginPath();
+    // ctx.moveTo(0, 0);
+    // ctx.lineTo(50,0);
+    // ctx.lineTo(50, -50);
+    // ctx.lineTo(0, -50);
+    ctx.moveTo(newCoords[0][0], newCoords[0][1]);
+    ctx.lineTo(newCoords[1][0], newCoords[1][1]);
+    ctx.lineTo(newCoords[2][0], newCoords[2][1]);
+    ctx.lineTo(newCoords[3][0], newCoords[3][1]);
+
+    ctx.closePath();
+    ctx.fill();
+}
+
+
+function calculateTransformation (matrix, ogCoords) {
+    const newCoords = new Array(2)
+    newCoords[0] = ogCoords[0] * matrix[0][0] + ogCoords[1] * matrix[0][1] + matrix[0][2]
+    newCoords[1] = ogCoords[0] * matrix[1][0] + ogCoords[1] * matrix[1][1] + matrix[1][2]
+    return newCoords
+}
+
+function multiplyMatrices(A, B) {
+    let rowsA = A.length;
+    let colsA = A[0].length;
+    let rowsB = B.length;
+    let colsB = B[0].length;
+    let C = new Array(rowsA);
+    for (let i = 0; i < rowsA; i++) {
+      C[i] = new Array(colsB);
+      for (let j = 0; j < colsB; j++) {
+        C[i][j] = 0;
+        for (let k = 0; k < colsA; k++) {
+          C[i][j] += A[i][k] * B[k][j];
+        }
+      }
+    }
+    return C;
+  }
+
+  function loadMatrix() {
+    let matrix = new Array(3)
+    m = document.getElementById("m1")
+  //  console.log(document.getElementById("m1").rows[0].cells[2].children[0].value)
+    for(let i = 0; i < 2; i++){
+        matrix[i] = new Array(3)
+        for(let j = 0; j < 3; j++){
+            matrix[i][j]  = +m.rows[i].cells[j].children[0].value
+        }
+    }
+    matrix[2] = [0,0,1]
+
+    return matrix
+    }
